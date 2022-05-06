f99e92037ca4bc8217df0f01dfe7472a4c6e7cbc
Victor Westerlund <victor@victorwesterlund.com>
2022-05-06 14:42:37 +0000

    Remove unused permission and add new PIP icon (#3)
    * feat: remove unused permission and new pip icon
    
    * Update README.md

--- chrome/assets/js/script.js
+++ chrome/assets/js/script.js
@@ -45,7 +45,7 @@ class PIPController {
 		pip.tabindex = "0";
 		pip.classList = "control-icon-btn fullscreen-icon";
 
-		pip.innerHTML = `<div class="focus-hack-div " tabindex="-1"><svg tabindex="-1" focusable="false"><use xlink:href="#067--arrow-down" style="transform: rotate(180deg) scale(0.6);transform-origin: 50% 50%;"></use><use xlink:href="#014--fullscreen"></use></svg></div>`;
+		pip.innerHTML = `<div class="focus-hack-div " tabindex="-1"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 -1 27 27" tabindex="-1" focusable="false"><path fill="#ffffff" d="M19 11h-8v6h8v-6zm4 8V4.98C23 3.88 22.1 3 21 3H3c-1.1 0-2 .88-2 1.98V19c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2zm-2 .02H3V4.97h18v14.05z"/></svg></div>`;
 
 		pip.addEventListener("click", () => this.togglePictureInPicture());
 
